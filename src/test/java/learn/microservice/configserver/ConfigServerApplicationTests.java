66bbd819142d5b15a4a0654b4f56295af4976e6e
Thanushree <thanushree@swayaan.com>
2024-01-02 12:27:35 +0530

    first commit

---
+++ src/test/java/learn/microservice/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.microservice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
